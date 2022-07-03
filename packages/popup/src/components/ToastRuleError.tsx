ca6f9b7429387481e41b173a412cb3599efeed4e
myWsq <wsq961@outlook.com>
2022-07-03 12:01:24 +0800

    style: toast light style

--- packages/popup/src/components/ToastRuleError.tsx
+++ packages/popup/src/components/ToastRuleError.tsx
@@ -7,5 +7,9 @@ export const ToastRuleError: React.FC = () => {
   if (!error) {
     return null;
   }
-  return <div className="px-4 py-1 text-sm text-red-500 bg-neutral-800/90">{error}</div>;
+  return (
+    <div className="px-4 py-1 text-sm text-red-500 bg-neutral-200/90 dark:bg-neutral-800/90">
+      {error}
+    </div>
+  );
 };
